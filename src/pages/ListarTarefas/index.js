28461e9af1f1ca4a2cc78f719a3917879d38dfa5
yago-codev <yagomilano92@gmail.com>
2020-04-19 19:19:54 -0300

    Utilizando o hook useEffect para carregar as tarefas do localStorage dentro da página ListarTarefas

--- src/pages/ListarTarefas/index.js
+++ src/pages/ListarTarefas/index.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { A } from "hookrouter";
+import { Container, Table } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+import "./style.css";
 
 export default function ListarTarefas() {
+  const [tarefas, setTarefas] = useState([]);
+  const [carregarTarefas, setCarregarTarefas] = useState(true);
+
+  useEffect(() => {
+    function obterTarefas() {
+      const tarefasDB = localStorage["tarefas"];
+
+      let listarTarefas = tarefasDB ? JSON.parse(tarefasDB) : [];
+
+      console.log(listarTarefas);
+      setTarefas(listarTarefas);
+    }
+
+    if (carregarTarefas) {
+      obterTarefas();
+      setCarregarTarefas(false);
+    }
+
+  }, [carregarTarefas]);
+
   return (
-    <A href="/cadastrar" className="btn btn-success btn-sm">
-      Nova Tarefa
-    </A>
+    <Container className="mt-5">
+      <Table striped hover responsive id="tabela" test-id="tabela">
+        <thead>
+          <tr>
+            <th>Tarefa</th>
+            <th>
+              <A
+                href="/cadastrar"
+                className="btn btn-sm"
+                data-testid="btn-link-nova-tarefa"
+              >
+                <FontAwesomeIcon icon={faPlus} />
+                &nbsp; Nova tarefa
+              </A>
+            </th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </Table>
+    </Container>
   );
 }
